08ef3f9920ee9029d64e4c1c362a1cb67e049b19
QKhanhGT6 <dinhquockhanhsiu@gmail.com>
2025-09-16 08:51:13 +0000

    feat: add new siglip beta service

---
+++ routes/SIGLIP_v2_B_router.py
@@ -0,0 +1,87 @@
+from fastapi import APIRouter
+
+from pathlib import Path
+import sys
+
+current_path = Path(__file__).resolve()
+for parent in current_path.parents:
+    if parent.name == "SIU_Pumpking":
+        #print(f"Adding {parent} to sys.path")
+        sys.path.append(str(parent))
+        break
+else:
+    raise RuntimeError("Could not find 'SIU_Pumpking' in the path hierarchy.")
+
+from handlers.SIGLIP_v2_B_handler import SIGLIPV2BetaHandler
+from utils.logger import get_logger
+
+logger = get_logger()
+
+
+def setup_router(handler: SIGLIPV2BetaHandler) -> APIRouter:
+    logger.info("setting up routers...")
+
+    router = APIRouter()
+
+    logger.info("setting up SIGLIP Beta vector retrieval router...")
+    siglip_router = APIRouter()
+
+    # Health check
+    siglip_router.add_api_route(
+        "/ping",
+        endpoint=handler.ping_handler,
+        methods=["GET"],
+    )
+
+    siglip_router.add_api_route(
+        "/setup_database",
+        endpoint=handler.setup_database_handler,
+        methods=["GET"]
+    )
+
+    # # Preprocess text to feature
+    # siglip_router.add_api_route(
+    #     "/preprocess",
+    #     endpoint=handler.preprocess_handler,
+    #     methods=["POST"],
+    # )
+
+    # Scroll video segments
+    siglip_router.add_api_route(
+        "/scroll",
+        endpoint=handler.scroll_handler,
+        methods=["POST"],
+    )
+
+    # Text-based vector search
+    siglip_router.add_api_route(
+        "/text_search",
+        endpoint=handler.text_search_handler,
+        methods=["POST"],
+    )
+
+    # Image-based vector search
+    siglip_router.add_api_route(
+        "/image_search",
+        endpoint=handler.image_search_handler,
+        methods=["POST"],
+    )
+
+    # Temporal (multi-segment) text search
+    siglip_router.add_api_route(
+        "/temporal_search",
+        endpoint=handler.temporal_search_handler,
+        methods=["POST"],
+    )
+
+    logger.info("SIGLIP v2 beta router setup successfully")
+
+    logger.info("adding routers...")
+    router.include_router(
+        siglip_router,
+        prefix="/siglip_v2_beta",
+        tags=["SIU_Pumpking"],
+    )
+    logger.info("routers setup successfully")
+
+    return router
---
+++ services/SIGLIP_v2_B_service.py
@@ -0,0 +1,64 @@
+import dotenv
+dotenv.load_dotenv()
+import uvicorn
+import signal
+import os
+from fastapi.middleware.gzip import GZipMiddleware 
+
+from pathlib import Path
+import sys
+
+current_path = Path(__file__).resolve()
+for parent in current_path.parents:
+    if parent.name == "SIU_Pumpking":
+        #print(f"Adding {parent} to sys.path")
+        sys.path.append(str(parent))
+        break
+else:
+    raise RuntimeError("Could not find 'SIU_Pumpking' in the path hierarchy.")
+
+from configs.SIGLIP_v2_B_configs import SIGLIPV2BetaConfig
+from handlers.SIGLIP_v2_B_handler import SIGLIPV2BetaHandler
+from routes.SIGLIP_v2_B_router import setup_router
+from utils.logger import get_logger
+from apis.api import setup_app, TimeoutMiddleware
+
+from engine.CLIPFeatureModel.siglip2_model import SIGLIP2
+from engine.vector_database.qdrant_database import QDRANT
+
+
+logger = get_logger()
+
+# Engine
+model = SIGLIP2()
+qdrant = QDRANT(SIGLIPV2BetaConfig().SIGLIP_V2_DATABASE_NAME) #SIGLIPV2BetaConfig().SIGLIP_V2_B_QDRANT_HOST
+
+app = setup_app()
+
+# Handlers
+vector_retrieval_handler = SIGLIPV2BetaHandler(qdrant_database = qdrant,
+                                                        model = model)
+
+# Routes
+router = setup_router(handler = vector_retrieval_handler)
+app.include_router(router)
+
+#app.add_middleware(TimeoutMiddleware, timeout=SIGLIPVectorRetrievalConfig().REQUEST_TIMEOUT)
+if os.getenv("ENABLE_GZIP", "True").lower() == "true":
+    app.add_middleware(GZipMiddleware, minimum_size=0)  # compress response > 0 bytes
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host=SIGLIPV2BetaConfig().SIGLIP_V2_HOST,
+        port=SIGLIPV2BetaConfig().SIGLIP_V2_PORT,
+        workers=SIGLIPV2BetaConfig().SIGLIP_V2_MAX_WORKERS, 
+        timeout_keep_alive=SIGLIPV2BetaConfig().TIMEOUT_KEEP_ALIVE
+    )
+    
+# Signal handling for graceful shutdown   
+def handle_sigterm(*args):
+    print("Received termination signal. Cleaning up...")
+    sys.exit(0)
+signal.signal(signal.SIGINT, handle_sigterm)
+signal.signal(signal.SIGTERM, handle_sigterm)
