d796658d8b88b36debec2f2325d58588cf055777
QKhanhGT6 <dinhquockhanhsiu@gmail.com>
2025-11-01 11:44:33 +0700

    feat: DRES - add schemas for submit handlers

--- schema/submission.py
+++ schema/submission.py
@@ -1,10 +1,22 @@
 from pydantic import BaseModel
 from typing import Optional
-
-
-class SubmitRequest(BaseModel):
+    
+class SubmitKISRequest(BaseModel):
     session_id: str
     eval_id: str
     mediaItemName: str
     start: int
     end: int
+    
+class SubmitQARequest(BaseModel):
+    session_id: str
+    eval_id: str
+    answer: str
+    video_id: str
+    time: str
+    
+class SubmitTRAKERequest(BaseModel):
+    session_id: str
+    eval_id: str
+    video_id: str
+    frame_ids: str
